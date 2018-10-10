3aa55cb7bdcbe83775c302479c8c22c5f796eab5
Anderson De Jesus Mejia Tapia <anderson_the_classic@hotmail.com>
2018-10-10 09:55:30 -0400

    Show more and Show less button functionality is working for album info section. I am using jquery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-    $(".show").click(function(){
-        $(".js--show-more").toggle();
+    
+    // Show more and Show less <button> functionality
+    $('#toggleButton').click(function() {
+        var button = this;
+        $('.js--show-more').slideToggle('slow', function() {
+            if($('.js--show-more').is(':visible')) {
+                $('span', button).text('... Show less');
+            }
+            else {
+                $('span', button).text('Show more ...');
+            }
+        });    
     });
+    
     
 });
